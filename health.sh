56128eeceb15d0a11d935512a4e79c3376b8f9f2
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-14 11:22:23 +0000

    if condition used

--- health.sh
+++ health.sh
@@ -2,7 +2,7 @@
 
 DATE=$(date)
 
-hostname=$(hostname)
+HOSTNAME=$(hostname)
 
 
 
@@ -10,10 +10,20 @@ echo "System health check"
 
 echo "Date: $DATE"
 
-echo "Hostname: $hostname"
+echo "Hostname: $HOSTNAME"
 
 echo "------------------"
 
+
+USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+
+echo "USAGE: $USAGE"
+
+if [ $USAGE -gt 80 ]; then
+	echo "Warning: Disk usage is high"
+else
+	echo "Disk Usage is normal"
+fi
 
 echo "Disk Usage"
 df -h
@@ -22,7 +32,7 @@ echo "Memory Usage"
 free -m
 
 echo "CPU usage"
-top
+top -b -n 1 | head -5
 
 echo "Running processes"
 ps aux | head -5
