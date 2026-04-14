78283a58dfbec538bcee41d3e3634dfdcb216057
EC2 Default User <ec2-user@ip-172-31-18-211.eu-north-1.compute.internal>
2026-04-14 09:51:54 +0000

    Initial commit: health script

---
+++ health.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "System ealth check"
+echo "------------------"
+
+
+echo "Disk Usage"
+df -h
+
+echo "Memory Usage"
+free -m
+
+echo "CPU usage"
+top
+
+echo "Running processes"
+ps aux | head -5
